afda86aa385270bee55dd1f645ae5d0036584d55
Matheus Alves <matheusrike.alves@gmail.com>
2025-08-12 17:06:54 -0300

    feat(ticket): add filters on find all and changes on update

--- backend/src/models/Ticket.model.js
+++ backend/src/models/Ticket.model.js
@@ -1,3 +1,4 @@
+import { th } from 'zod/locales';
 import prisma from '../config/prisma-client.js';
 
 export class Ticket {
@@ -15,7 +16,11 @@ export class Ticket {
 		try {
 			if (patrimony_id) {
 				const existing = await prisma.ticket.findFirst({
-					where: { patrimony_id, status: { not: 'completed' } },
+					where: {
+						patrimony_id,
+						category_id,
+						status: { not: 'completed' },
+					},
 				});
 
 				if (existing) throw new Error('DUPLICATED_TICKET');
@@ -32,17 +37,40 @@ export class Ticket {
 				select: this._baseSelect,
 			});
 		} catch (error) {
+			if (error.message === 'DUPLICATED_TICKET') throw error;
 			throw new Error(`Error creating ticket: ${error}`);
 		}
 	}
 
-	static async findAll({ userId, role }, { categoryId, patrimonyId }) {
-		const where = {};
+	static async findAll(
+		{ userId, role },
+		{
+			categoryId,
+			patrimonyId,
+			status,
+			technicianId,
+			createdAfter,
+			createdBefore,
+		},
+	) {
+		let where = {};
 		if (role === 'technician') where.technician_id = userId;
 		if (role === 'user') where.user_id = userId;
 
-		if (categoryId) where.category_id = categoryId;
-		if (patrimonyId) where.patrimony_id = patrimonyId;
+		const filters = {
+			...(categoryId && { category_id: categoryId }),
+			...(patrimonyId && { patrimony_id: patrimonyId }),
+			...(status && { status: status }),
+			...(technicianId && { technician_id: technicianId }),
+		};
+
+		if (createdBefore || createdAfter) {
+			filters.created_at = {};
+			if (createdAfter) filters.created_at.gte = new Date(createdAfter);
+			if (createdBefore) filters.created_at.lte = new Date(createdBefore);
+		}
+
+		where = { ...where, ...filters };
 
 		return await prisma.ticket.findMany({
 			where,
@@ -68,7 +96,9 @@ export class Ticket {
 		return ticket;
 	}
 
-	static async update({ ticketId, data }) {
+	static async update({ ticketId, data, role }) {
+		if (role !== 'admin') throw new Error('FORBIDDEN');
+
 		try {
 			return await prisma.ticket.update({
 				where: { id: ticketId },
@@ -81,17 +111,23 @@ export class Ticket {
 		}
 	}
 
-	async updateStatus({ status }) {
-		try {
-			return await prisma.ticket.update({
-				where: { id: this.id },
-				data: { status },
-				select: this._baseSelect,
-			});
-		} catch (error) {
-			if (error.code === 'P2025') throw new Error('NOT_FOUND');
-			throw new Error(`Error updating ticket: ${error}`);
+	async updateStatus({ status, userId, role }) {
+		const ticket = await prisma.ticket.findUnique({
+			where: { id: this.id },
+			select: this._baseSelect,
+		});
+
+		if (!ticket) throw new Error('NOT_FOUND');
+
+		if (role !== 'admin' && ticket.technician_id !== userId) {
+			throw new Error('FORBIDDEN');
 		}
+
+		return await prisma.ticket.update({
+			where: { id: this.id },
+			data: { status },
+			select: this._baseSelect,
+		});
 	}
 
 	static _baseSelect = {
--- backend/src/schemas/ticket.schema.js
+++ backend/src/schemas/ticket.schema.js
@@ -4,7 +4,12 @@ import { idSchema } from './generic.schema.js';
 export const createTicket = z.object({
 	title: z.string('Title is required'),
 	description: z.string('Description is required'),
-	user_id: idSchema('User ID is required'),
 	category_id: idSchema('Category ID is required'),
 	patrimony_id: idSchema().optional(),
+});
+
+export const updateTicket = z.object({
+	title: z.string().optional(),
+	description: z.string().optional(),
+	category_id: idSchema().optional(),
 });
