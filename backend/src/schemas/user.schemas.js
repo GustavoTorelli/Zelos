28b0d0646b4590f84adb815f6a602f2d6d530530
Matheus Alves <matheusrike.alves@gmail.com>
2025-08-14 16:06:01 -0300

    fix: add conversion to lower case in user email validation

--- backend/src/schemas/user.schemas.js
+++ backend/src/schemas/user.schemas.js
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const userCreateSchema = z.object({
 	name: z.string().min(3, 'Name must be at least 3 characters long.'),
-	email: z.email('Invalid email address.'),
+	email: z.email('Invalid email address.').toLowerCase(),
 	password: z.string().min(6, 'Password must be at least 6 characters long.'),
 	role: z.enum(
 		['user', 'admin', 'technician'],
