9de211849ea165472ce58cd9cba3ac7f9a9756c1
Matheus <matheusrike.alves@gmail.com>
2025-08-25 17:08:16 -0300

    feat: add categories seeding functionality and reorganize seed files

---
+++ backend/src/config/seeds/admin.seed.js
@@ -0,0 +1,33 @@
+import { ZodError } from 'zod';
+import prisma from '../prisma-client.js';
+import hashPassword from '../../utils/hash-password.js';
+import { emailSchema } from '../../schemas/generic.schema.js';
+
+export default async function seedAdmin() {
+	try {
+		const adminData = {
+			name: 'admin',
+			email: emailSchema.parse(process.env.ADMIN_EMAIL),
+			hashed_password: await hashPassword(process.env.ADMIN_PASSWORD),
+			role: 'admin',
+			is_active: true,
+		};
+
+		await prisma.user.upsert({
+			where: { email: adminData.email },
+			update: {},
+			create: adminData,
+		});
+
+		console.log(
+			`✔ Admin user created with credentials:\nemail: ${adminData.email} | password: ${process.env.ADMIN_PASSWORD}`,
+		);
+	} catch (error) {
+		if (error instanceof ZodError) {
+			console.log('Error seeding admin: Invalid email address');
+			return;
+		}
+
+		console.error('Error seeding admin:', error);
+	}
+}
---
+++ backend/src/config/seeds/categories.seed.js
@@ -0,0 +1,76 @@
+import prisma from '../prisma-client.js';
+import { createCategory } from '../../schemas/category.schema.js';
+import { ZodError } from 'zod';
+
+export default async function seedCategories() {
+	try {
+		const initial_categories = [
+			{
+				title: 'Manutenção Elétrica',
+				description:
+					'Falhas de energia, problemas em tomadas, iluminação e quadros elétricos.',
+			},
+			{
+				title: 'Manutenção Hidráulica',
+				description:
+					'Vazamentos, entupimentos e problemas em torneiras.',
+			},
+			{
+				title: 'Manutenção Predial',
+				description: 'Reparos em paredes, pisos, portas e telhados.',
+			},
+			{
+				title: 'TI - Hardware',
+				description:
+					'Falhas em computadores, notebooks, impressoras e periféricos.',
+			},
+			{
+				title: 'TI - Software',
+				description:
+					'Instalação, atualização ou falha de softwares e sistemas.',
+			},
+			{
+				title: 'TI - Rede',
+				description:
+					'Problemas de conectividade, Wi-Fi e acesso à internet.',
+			},
+			{
+				title: 'Equipamentos de Laboratório',
+				description:
+					'Problemas com máquinas CNC, bancadas de teste, tornos e equipamentos específicos.',
+			},
+			{
+				title: 'Suporte Acadêmico',
+				description:
+					'Solicitação de materiais, reserva de salas e problemas de impressão.',
+			},
+		];
+
+		let valid_categories = [];
+
+		for (const category of initial_categories) {
+			valid_categories.push(createCategory.parse(category));
+		}
+
+		for (const category of valid_categories) {
+			await prisma.category.upsert({
+				where: { title: category.title },
+				update: {},
+				create: {
+					...category,
+					created_by: 1,
+					updated_by: 1,
+				},
+			});
+		}
+
+		console.log('✔ Categories seeded successfully');
+	} catch (error) {
+		if (error instanceof ZodError) {
+			console.error('Error seeding categories: Invalid category data');
+			return;
+		}
+
+		console.error('Error seeding categories:', error);
+	}
+}
